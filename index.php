4b167a9a051fbabf28d14c1a83f4a5925f547971
Sakey <starikovartem1991@yandex.ru>
2024-02-24 22:07:45 +0300

    Тест fix

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+   <link href="css/style.css" rel="stylesheet">  
+<head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Document</title>
+</head>
+<body>
+   <h1>Hello</h1>
+   <!-- !ddsds    -->
+</body>
+<div class=""></div>
+</html>
