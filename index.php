b72136a3a8ab74a95479df562426d94c5b2f26d2
Sakey <starikovartem1991@yandex.ru>
2024-02-24 22:11:07 +0300

    Исправления

--- index.php
+++ index.php
@@ -1,14 +1,13 @@
 <!DOCTYPE html>
-<html lang="en">
-   <link href="css/style.css" rel="stylesheet">  
+<html lang="en"> 
 <head>
    <meta charset="UTF-8">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <title>Document</title>
+   <link href="css/style.css" rel="stylesheet"> 
 </head>
 <body>
    <h1>Hello</h1>
-   <!-- !ddsds    -->
 </body>
 <div class=""></div>
 </html>
